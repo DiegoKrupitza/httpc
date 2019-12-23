aee496ac62eac7541ed25eb4e80c80160affe803
Diego Krupitza <info@diegokrupitza.com>
2019-12-24 00:43:28 +0100

    getting the request from the client

--- server.c
+++ server.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <string.h>
 
 #include "server.h"
 
@@ -71,8 +72,77 @@ void readArguments(int argc, char *argv[], serverarguments* args)
     }
 }
 
-void startServer(serverarguments* args) {
+int startServer(serverarguments* args) {
 
+    struct addrinfo hints, *ai;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    int res = getaddrinfo(NULL, args->port, &hints, &ai);
+    if (res != 0)
+    {
+        // error
+        fprintf(stderr, "./%s: Error while getaddrinfo: %s\n", PROGRAMNAME, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    int sockfd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+    if (sockfd < 0)
+    {
+        // error
+        fprintf(stderr, "./%s: Error while socket generation: %s\n", PROGRAMNAME, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // allow to reuse the port
+    int optval = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval) < 0)
+    {
+        // error
+        fprintf(stderr, "./%s: Error while setting socketoptions: %s\n", PROGRAMNAME, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (bind(sockfd, ai->ai_addr, ai->ai_addrlen) < 0)
+    {
+        // error
+        fprintf(stderr, "./%s: Error while binding socket to port: %s\n", PROGRAMNAME, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    if (listen(sockfd, DEFAULT_CLIENT_LISTEN_SIZE) < 0)
+    {
+        // error
+        fprintf(stderr, "./%s: Error while listening to port: %s\n", PROGRAMNAME, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    freeaddrinfo(ai);
+    return sockfd;
+}
+
+void clientWaiting(int serverFd) {
+    while(1) {
+        debug("In request\n");
+        int clientFD = accept(serverFd, NULL, NULL);
+        if (clientFD < 0)
+        {
+            // error
+            fprintf(stderr, "./%s: Error accepting new client: %s\n", PROGRAMNAME, strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        FILE *clientFd = fdopen(clientFD, "r");
+
+        char *line = NULL;
+        size_t linecap = 0;
+        ssize_t linelen;
+        while ((linelen = getline(&line, &linecap, clientFd)) > 0) {
+                fwrite(line, linelen, 1, stdout);
+                fflush(stdout);
+        }
+
+        fclose(clientFd);
+    }
 }
 
 int main(int argc, char *argv[])
@@ -81,7 +151,8 @@ int main(int argc, char *argv[])
         .port = DEFAULTPORT};
 
     readArguments(argc, argv, &args);
-    startServer(&args);
+    int serverFd = startServer(&args);
+    clientWaiting(serverFd);
 
     return EXIT_SUCCESS;
 }
--- server.h
+++ server.h
@@ -1,12 +1,11 @@
 #define PROGRAMNAME "httpc"
 #define DEFAULTPORT "80"
+#define DEFAULT_CLIENT_LISTEN_SIZE 1
 
 #define debug(fmt, ...) \
 (void) fprintf(stderr, "[%s:%d] " fmt "\n", \
                  __FILE__, __LINE__, \
                  ##__VA_ARGS__)
-
-
 
 typedef struct
 {
